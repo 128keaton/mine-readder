9a7cef7efdabf9c9fd80cc79334a65611803d76b
Keaton Burleson <keaton.burleson@me.com>
2015-07-18 19:36:35 -0500

    make it better

--- app/app.py
+++ app/app.py
@@ -95,7 +95,6 @@ def server_static(filename):
 schedule_update()
 schedule_json()
 
-if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     run(server='gevent', host='0.0.0.0', port=port)
